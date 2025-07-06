7b89bd644632840d998382092f73068390b93254
Manpreet Singh <ms0014460@gmail.com>
2025-07-06 20:32:18 +0530

    Initial backend commit

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,6 @@ app.use("/api/ai/generate-explanation",protect,generateConceptExplanation);
 
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
